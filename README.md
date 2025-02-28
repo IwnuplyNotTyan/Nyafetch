34f822f796878165f4c57c80095bf45003440141
iwnuplynottyan <wolflast21@gmail.com>
2025-02-28 16:07:13 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Nyafetch
+# 💖 ~ *Nyafetch*
+
+![image](https://gachi.gay/_iRiX)
+
+## Build
+```sh
+go mod download
+go build -o nyafetch main.go
+```
+
+### Build with ❤️
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/muesli/termenv"
+)
+
+func getDistro() string {
+	data, err := os.ReadFile("/etc/os-release")
+	if err != nil {
+		return "Unknown"
+	}
+
+	for _, line := range strings.Split(string(data), "\n") {
+		if strings.HasPrefix(line, "PRETTY_NAME=") {
+			return strings.Trim(line[12:], "\"")
+		}
+	}
+	return "Unknown"
+}
+
+func getKernelVersion() string {
+	out, err := exec.Command("uname", "-r").Output()
+	if err != nil {
+		return "Unknown"
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func getWM() string {
+	out, err := exec.Command("ps", "-e", "-o", "comm=").Output()
+	if err != nil {
+		return "Unknown"
+	}
+
+	processList := strings.Split(string(out), "\n")
+	wmList := []string{"i3", "openbox", "kwin_x11", "mutter", "xfwm4", "metacity", "compiz", "fluxbox", "bspwm", "awesome", "dwm"}
+
+	for _, proc := range processList {
+		trimmedProc := strings.TrimSpace(proc)
+		for _, wm := range wmList {
+			if trimmedProc == wm {
+				return wm
+			}
+		}
+	}
+	return "Unknown"
+}
+
+func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error getting hostname:", err)
+		return
+	}
+
+	hn := termenv.String(fmt.Sprint("hn ")).Foreground(termenv.ANSIBlue)
+	kr := termenv.String(fmt.Sprint("kr ")).Foreground(termenv.ANSIRed)
+	os := termenv.String(fmt.Sprint("os ")).Foreground(termenv.ANSIGreen)
+	wm := termenv.String(fmt.Sprint("wm ")).Foreground(termenv.ANSICyan)
+
+	fmt.Println("  ／l、    ", os, getDistro())
+	fmt.Println("（ﾟ､ ｡ ７  ", wm, getWM())
+	fmt.Println(" l  ~ ヽ   ", kr, getKernelVersion())
+	fmt.Println(" じしf_,)ノ", hn, hostname)
+}
